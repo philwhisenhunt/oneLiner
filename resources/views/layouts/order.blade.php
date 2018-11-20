e9e75a031cfae07f3ad53e5af5fb0f8219ae1639
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-11-20 10:40:34 -0500

    Fixed submit button not working, Dustin caught that there were two forms.

--- resources/views/layouts/order.blade.php
+++ resources/views/layouts/order.blade.php
@@ -19,7 +19,7 @@
              
             </div>
 
-            <form action="POST">
+           
               <div class="row">
                 <div class="col-md-12 mb-3">
                   <input type="text" class="form-control" id="email" name="email" placeholder="Valid email address" required>
@@ -29,7 +29,7 @@
                   
                 </div>
               </div>
-            </form>
+            
           </div>
 
 <!--
